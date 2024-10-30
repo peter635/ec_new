3e1ecbda3a8611679d2998b12827f6d226b38eed
peter ma <peter635@gmail.com>
2024-10-30 14:01:58 +0900

    adding GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,20 @@ module.exports = {
     title: `Gatsby Sydney Ecommerce Theme`,
     siteUrl: `https://jamm.matter.design`,
   },
-  plugins: [],
+  plugins: [
+
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-PNFBGFRC', // 替换为你的 GTM 容器 ID
+        includeInDevelopment: true,
+        defaultDataLayer: { platform: 'gatsby' },
+        // 可选：指定 GTM 脚本的路径
+       // gtmAuth: 'YOUR_GTM_ENV_AUTH_STRING',
+        //gtmPreview: 'YOUR_GTM_ENV_PREVIEW_NAME',
+        dataLayerName: 'dataLayer',
+      },
+    },
+    // 其他插件...
+  ],
 };
